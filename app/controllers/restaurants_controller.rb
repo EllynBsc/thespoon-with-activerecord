66a7ed60cf056ce1936d7e3bfb19bba1b328a8b2
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-09-17 10:42:28 +0200

    lecture finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,53 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit]
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     # raise
-    @restaurant = Restaurant.find(params[:id])
+    # @restaurant = Restaurant.find(params[:id])
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
-    @restaurant.destroy
+    restaurant = Restaurant.find(params[:id])
+    restaurant.destroy
     redirect_to restaurants_path
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    # raise (create is the same as new +save)
+    # raise
+    # restaurant = Restaurant.create(name: params[:name],address: params[:address], stars: params[:stars])
+    # restaurant = Restaurant.create(params[:restaurant])
+    restaurant = Restaurant.create(restaurant_strong_params)
+    redirect_to restaurant_path(restaurant)
+  end
+
+  def edit
+    # raise
+    # @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    # raise
+    restaurant = Restaurant.find(params[:id])
+    restaurant.update(restaurant_strong_params)
+    redirect_to restaurant_path(restaurant)
+  end
+
+
+  private
+
+  def restaurant_strong_params
+    params.require(:restaurant).permit(:name, :address, :stars, :admin)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,23 @@ Rails.application.routes.draw do
 
   # CRUD
   # CREATE
+  # 2 request
+  # a get request to get a form, an empty instance of the restaurant model
+  get '/restaurants/new', to: 'restaurants#new'
+  # a post request to save the restauranst instance in the DB passing the params of the user
+  post '/restaurants', to: 'restaurants#create'
+
   # READ ALL
   get '/restaurants', to: 'restaurants#index'
   # READ ONE
   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 
   # UPDATE
-  # DELETE
+  # edit => form
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # update => change the db
+  patch '/restaurants/:id', to: 'restaurants#update'
 
+  # DELETE
   delete '/restaurants/:id', to: 'restaurants#destroy'
 end
